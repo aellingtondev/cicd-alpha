f4a31ba072d54407bd58563601af5f74decb5759
root <root@DESKTOP-ANFOMF3.localdomain>
2022-09-09 18:47:46 -0400

    first commit windows attempt

--- provision.sh
+++ provision.sh
@@ -1,5 +1,9 @@
-apt-get -y update
+apt-get update
 
-apt-get -y install nginx
+apt-get install -y curl openssh-server ca-certificates tzdata perl
 
-service nginx start
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+EXTERNAL_URL="http://localhost" apt-get install gitlab-ce
+
+
